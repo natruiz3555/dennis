ae943d8eace55b5644668a5c255d68d399139d38
Nathan Ruiz <natruiz3553@gmail.com>
2014-09-26 22:17:07 +1000

    Added old networkManager.py file

--- networkManager.py
+++ networkManager.py
@@ -4,7 +4,6 @@ import DataTypes
 import time
 import packetDispatch
 
-
 buff = DataTypes.Buffer()
 
 HOST = 'localhost'
@@ -13,44 +12,41 @@ PORT = 25565
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 s.setblocking(0)
-
 def writeLength(data):
-    return DataTypes.writeVarInt(len(data)) + data
+	return DataTypes.writeVarInt(len(data)) + data
 
 def login():
-    global sendData
-    packet = "\x00"
-    packet += DataTypes.writeVarInt(47)
-    packet += DataTypes.writeString(HOST)
-    packet += DataTypes.writeUnsignedShort(PORT)
-    packet += DataTypes.writeVarInt(2)
-    packet = writeLength(packet)
-    sendData += packet
-    #Next Packet
-    packet = '\x00'
-    packet += DataTypes.writeString("jem50")
-    packet = writeLength(packet)
-    sendData += packet
+	global sendData
+	packet = "\x00"
+	packet += DataTypes.writeVarInt(47)
+	packet += DataTypes.writeString(HOST)
+	packet += DataTypes.writeUnsignedShort(PORT)
+	packet += DataTypes.writeVarInt(2)
+	packet = writeLength(packet)
+	packetDispatch.sendData += packet
+	#Next Packet
+	packet = '\x00'
+	packet += DataTypes.writeString("TheBot")
+	packet = writeLength(packet)
+	packetDispatch.sendData += packet
 
 
 login()
-t=0
-while True:
-    try:
-        buff.addRaw(s.recv(16))
-    except socket.error, v:
-        pass
-    if sendData:
-        s.send(sendData)
-        sendData = ""
-    packet = buff.getNextPacket()
-    if packet:
-        a = hex(packet.readVarInt())
-        while len(a) < 4:
-            a = "0x0" + a[2:]
-        a = a.replace("f", "F")
-        print("+" + a)
-        pDispatch[a](packet)
 
+while True:
+	try:
+		buff.addRaw(s.recv(1024))
+	except socket.error, v:
+		pass
+	if packetDispatch.sendData:
+		s.send(packetDispatch.sendData)
+		packetDispatch.sendData = ""
+	packet = buff.getNextPacket()
+	if packet:
+		a = hex(packet.readVarInt())
+		while len(a) < 4:
+			a = "0x0" + a[2:]
+		a = a.upper().replace("X", "x")
+		packetDispatch.pDispatch[a](packet)
 
 
